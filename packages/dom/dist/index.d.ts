429ec342d2aa5b83a3d3b1f1c0525be620aec387
koharxnp <korphere@DESKTOP-76AH8L4.localdomain>
2026-07-08 15:51:59 +0900

    fix: For arguments

--- packages/dom/dist/index.d.ts
+++ packages/dom/dist/index.d.ts
@@ -1,6 +1,6 @@
 import { Node as WvNode } from '@watervein/core';
 export declare function Show(condition: WvNode<boolean> | (() => boolean), thenFn: () => HTMLElement, elseFn?: () => HTMLElement): HTMLElement;
-export declare function For<T>(listNode: WvNode<T[]>, keyFn: (item: T) => any, renderFn: (getItem: () => T) => HTMLElement): HTMLElement;
+export declare function For<T>(listNode: WvNode<T[]>, keyFn: (item: T) => any, renderFn: (getItem: () => T) => HTMLElement, tagName?: string): HTMLElement;
 export { mount, mountToBody } from '@watervein/dom-core';
 export * from './elements.js';
 export * from './base.js';
--- packages/dom/dist/index.js
+++ packages/dom/dist/index.js
@@ -2,8 +2,8 @@ import { Show as show0, For as for0, } from '@watervein/dom-core';
 export function Show(condition, thenFn, elseFn) {
     return show0(condition, thenFn, elseFn);
 }
-export function For(listNode, keyFn, renderFn) {
-    return for0(listNode, keyFn, renderFn);
+export function For(listNode, keyFn, renderFn, tagName = "span") {
+    return for0(listNode, keyFn, renderFn, tagName);
 }
 export { mount, mountToBody } from '@watervein/dom-core';
 export * from './elements.js';
